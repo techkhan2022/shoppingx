14b747500e6d65285feb4de6aecb8fab89b13a7d
unknown <qassi8582@gmail.com>
2022-02-04 11:43:59 +0500

    last commit

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.views.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 
 # from django.views.static import serve
 # from django.conf.urls import url
@@ -24,9 +24,9 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app.urls')),
-    # url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    # url(r'^media/(?P<pleaseath>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
     # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
